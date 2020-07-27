ef8ae15e231127b6a45775bafcaf73d6720a2355
ali falahati <a.falahati@jiring.local>
2020-07-27 04:34:47 -0700

    docker test

--- docker/init-db.sql
+++ docker/init-db.sql
@@ -1,2 +1,13 @@
-# create testing database
 CREATE DATABASE IF NOT EXISTS `yektadg_db`;
+
+CREATE TABLE IF NOT EXISTS 'users'
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL ,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    avatar VARCHAR(250) NULL,
+    created_at DATE DEFAULT CURRENT_DATE() NOT NULL,
+    updated_at DATE DEFAULT CURRENT_DATE() NOT NULL
+) ENGINE=INNODB;;
